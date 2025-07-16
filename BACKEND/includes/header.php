6ec91fd1fe16326e8b225b73a875b308b4dff289
ZerkOner <luciano.cartarosa2@gmail.com>
2025-07-16 11:05:27 +0200

    DEBUG Home button

--- BACKEND/includes/header.php
+++ BACKEND/includes/header.php
@@ -1,4 +1,4 @@
 <header class="admin-header">
-  <a href="/qui_est_la/admin/dashboard.php" class="btn-home" title="Retour au tableau de bord">🏠 Accueil</a>
+  <a href="/qui_est_la/BACKEND/admin/dashboard.php" class="btn-home" title="Retour au tableau de bord">🏠 Accueil</a>
 </header>
 
